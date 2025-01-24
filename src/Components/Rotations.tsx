ebda711db110edb60658c7791435beeff6d084a3
Hue <aka@rien.fr>
2025-01-24 15:52:45 +0100

    separated Rotations and Rotation

--- src/Components/Rotations.tsx
+++ src/Components/Rotations.tsx
@@ -1,51 +1,22 @@
 import { useState, useEffect } from 'react';
+import Rotation from './Rotation';
 
-// Interfaces to define the structure of the response data. wi luv typescript
+// interfaces to define the structure of the response data, part 1 (or 2 if you prefer). wi luv typescript
 
-interface WeaponImage {
-  url: string;
-}
-
-interface Weapon {
-  name: string;
-  image: WeaponImage;
-}
-
-interface CoopStage {
-  name: string;
-  thumbnailImage: WeaponImage;
-  image: WeaponImage;
-}
-
-interface Boss {
-  name: string;
-  id: string;
-}
-
-interface CoopSetting {
-  boss: Boss
-  coopStage: CoopStage
-  weapons: Weapon[]
-}
-
-interface RegularSchedule {
-  startTime: string
-  endTime: string
-  setting: CoopSetting
-}
+interface RegularSchedule {}
 
 interface CoopGroupingSchedule {
   regularSchedules: {
-    nodes: RegularSchedule[]
+    nodes: RegularSchedule[];
   };
 }
 
-interface data {
-  coopGroupingSchedule: CoopGroupingSchedule
+interface Data {
+  coopGroupingSchedule: CoopGroupingSchedule;
 }
 
 interface ApiResponse {
-  data: data;
+  data: Data;
 }
 
 const Rotations = () => {
@@ -85,48 +56,9 @@ const Rotations = () => {
       <h1>Upcoming Coop Schedules</h1>
 
       {scheduleData && scheduleData.length > 0 ? (
-        scheduleData.map((schedule, index) => {
-          const { startTime, endTime, setting } = schedule;
-          const { boss, coopStage, weapons } = setting;
-
-          return (
-            <div key={index} className="schedule-item">
-              <h2>{coopStage.name}</h2>
-              <div>
-                <img
-                  src={coopStage.thumbnailImage.url}
-                  alt={coopStage.name}
-                  width={100}
-                  height={100}
-                />
-              </div>
-              <p>
-                <strong>Boss:</strong> {boss.name}
-              </p>
-              <p>
-                <strong>Start Time:</strong> {new Date(startTime).toLocaleString()}
-              </p>
-              <p>
-                <strong>End Time:</strong> {new Date(endTime).toLocaleString()}
-              </p>
-
-              <div className="weapons">
-                <h3>Weapons:</h3>
-                {weapons.map((weapon, idx) => (
-                  <div key={idx} className="weapon">
-                    <img
-                      src={weapon.image.url}
-                      alt={weapon.name}
-                      width={50}
-                      height={50}
-                    />
-                    <p>{weapon.name}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          );
-        })
+        scheduleData.map((schedule, index) => (
+          <Rotation key={index} schedule={schedule} />
+        ))
       ) : (
         <p>No schedules available at the moment.</p>
       )}
